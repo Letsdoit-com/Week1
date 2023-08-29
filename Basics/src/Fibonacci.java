64284872597d64cef18442c6312dc1143b8a132a
lokeshrookie <rocklinglokesh@gmial.com>
2023-08-29 23:09:36 +0530

    Basics

---
+++ Basics/src/Fibonacci.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Fibonacci {
+    public static void main(String[] args) {
+        int a = 0;
+        int b = 1;
+        Scanner s = new Scanner(System.in);
+        int n = s.nextInt();
+        int c = a+b;
+        for (int i = 0; i < n; i++) {
+            
+
+        }
+    }
+}
